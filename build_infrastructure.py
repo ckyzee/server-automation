4178baded47a0d56acb5265ccde89e8778eaeed9
Colin Zee <colinzee@Colins-MacBook-Pro-2.local>
2020-04-20 05:29:39 -0400

    updated

--- build_infrastructure.py
+++ build_infrastructure.py
@@ -1,5 +1,6 @@
 import git
 from datetime import datetime
 
-datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")
+
+
 git.Git("./freebsd_"+datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")).clone("https://github.com/freebsd/freebsd")
